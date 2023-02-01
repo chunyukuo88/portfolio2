5e79c6b396a8ac39e445775afb410441b34e8520
chunyukuo88 <alexgochenour@gmail.com>
2023-01-31 22:26:38 -0500

    temporary styling so that I can tell what is happening.

--- src/features/crossword/Crossword.jsx
+++ src/features/crossword/Crossword.jsx
@@ -1,24 +1,32 @@
-import * as React from 'react';
+import React from 'react';
+import './Crossword.css';
 
-export function Crossword(){
+export default function Crossword(){
   const emptyGrid = [
-    ['','','','',''],
-    ['','','','',''],
-    ['','','','',''],
-    ['','','','',''],
-    ['','','','',''],
+    ['o','o','o','o','o'],
+    ['o','o','o','o','o'],
+    ['o','o','o','o','o'],
+    ['o','o','o','o','o'],
+    ['o','o','o','o','o'],
   ];
   const [ grid, setGrid ] = React.useState(emptyGrid);
   return (
       <main>
-        <h1>Crossword</h1>
         <section>
+          <h1>Crossword</h1>
           {
-            emptyGrid.map(row => {
-              // row.map(square => {
-                return <div className='square'>ha</div>
-              // })
-            })
+            grid.map((row, outerIndex) => (
+              <div className='crossword-row' key={outerIndex}>
+                {
+                  row.map((square, innerIndex) => (
+                    <div key={innerIndex} className='crossword-square'>
+                      {square}
+                    </div>
+                    )
+                  )
+                }
+              </div>
+            ))
           }
         </section>
       </main>
--- src/features/crossword/Crossword.test.js
+++ src/features/crossword/Crossword.test.js
@@ -1,22 +1,19 @@
-import * as React from 'react';
+import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import { Crossword } from './Crossword.jsx';
-import '@testing-library/jest-dom'
+import Crossword from './Crossword.jsx';
+import '@testing-library/jest-dom';
+
 import 'react-dom';
 
 describe('GIVEN: The 5x5 crossword grid is empty,', ()=>{
   describe('WHEN: the user clicks on a square,', ()=>{
     it('THEN: the entire row is highlighted.', ()=>{
-      // const { debug } = render(Crossword());
-      const { debug } = render(<Crossword />);
-      //
+      render(<Crossword />);
+
       // const squares = document.querySelectorAll('.square');
       // const neighboringSquareLeft = squares[1];
       // const neighboringSquareRight = squares[3];
-      //
       // expect(neighboringSquareLeft).toHaveStyle('background-color: white');
-
-
       // const clickedSquare = squares[2];
       // fireEvent.click(clickedSquare);
 
