b40162a6531d00f7e92a5077662d3459f0cd136a
chunyukuo88 <alexgochenour@gmail.com>
2023-02-10 17:46:03 -0500

    more cleanup and adjustment for blog page.

--- src/pages/Blog/BlogPage.jsx
+++ src/pages/Blog/BlogPage.jsx
@@ -1,11 +1,22 @@
 import './BlogPage.css';
+import { Cube } from '../../components/Cube/Cube';
+import strings from '../../common/strings';
+import { useSelector } from 'react-redux';
 
 export function BlogPage(){
+  const language = useSelector((state) => state.language.value);
+
   return (
     <main>
-      <title className='blog-coming-soon'>
+      <div className='blog-coming-soon'>
         Blog feature coming soon!
-      </title>
+      </div>
+      <p>
+        <a className='back-to-home' href="/">{strings.homePage[language]}</a>
+      </p>
+      <div className='green-cube-wrapper'>
+        <Cube />
+      </div>
     </main>
   );
 }
