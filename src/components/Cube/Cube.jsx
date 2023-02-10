2ac6ef70dd83e40b9e791c9af5bd49958adc4c38
chunyukuo88 <alexgochenour@gmail.com>
2023-02-10 17:08:20 -0500

    glowing cube done. Next: Refactor to make color and rotation speed configurable.

---
+++ src/components/Cube/Cube.jsx
@@ -0,0 +1,17 @@
+import './Blog.css';
+
+export function Blog() {
+  return (
+    <div className="cube">
+      <div className='cube-lid'/>
+      <div>
+        <span />
+        <span />
+        <span />
+        <span />
+      </div>
+    </div>
+  );
+}
+
+
