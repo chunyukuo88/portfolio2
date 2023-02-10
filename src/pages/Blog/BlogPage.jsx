40f021c6f0b8fda335eceb1c3a6e79223188f988
chunyukuo88 <alexgochenour@gmail.com>
2023-02-10 17:26:34 -0500

    refactor Blog-related things. Also removed Counter feature and related global slice.

---
+++ src/pages/Blog/BlogPage.jsx
@@ -0,0 +1,11 @@
+import './BlogPage.css';
+
+export function BlogPage(){
+  return (
+    <main>
+      <title className='blog-coming-soon'>
+        Blog feature coming soon!
+      </title>
+    </main>
+  );
+}
