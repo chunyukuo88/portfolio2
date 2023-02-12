db8ccae357023d9fb2a1b8ee8f49d2e707b76eb9
chunyukuo88 <alexgochenour@gmail.com>
2023-02-12 17:55:00 -0500

    Updated test for BlogPage.jsx

--- src/pages/Blog/BlogPage.jsx
+++ src/pages/Blog/BlogPage.jsx
@@ -2,9 +2,14 @@ import './BlogPage.css';
 import { Cube } from '../../components/Cube/Cube';
 import strings from '../../common/strings';
 import { useSelector } from 'react-redux';
+import {useEffect} from "react";
+import ReactGA from "react-ga4";
 
 export function BlogPage(){
   const language = useSelector((state) => state.language.value);
+  useEffect(() => {
+    ReactGA.send({ hitType: 'pageview', page: '/blog' });
+  }, []);
 
   return (
     <main>
--- src/pages/Blog/BlogPage.test.js
+++ src/pages/Blog/BlogPage.test.js
@@ -1,15 +1,30 @@
 import { BlogPage } from './BlogPage';
 import { render } from '@testing-library/react';
 import { store } from '../../globalState/store';
+import ReactGA from 'react-ga4';
 import Root from '../../Root';
 
-it('The page renders properly', () => {
-  render(
-    <Root store={store}>
-      <BlogPage/>
-    </Root>
-  );
-  const component = document.querySelector('main');
+describe('WHEN: The page loads,', () => {
+  it('THEN: renders properly', () => {
+    render(
+      <Root store={store}>
+        <BlogPage/>
+      </Root>
+    );
+    const component = document.querySelector('main');
 
-  expect(component).toBeInTheDocument();
+    expect(component).toBeInTheDocument();
+  });
+  it('THEN: ReactGA sends info to Google Analytics.', () => {
+    const spy = jest.spyOn(ReactGA, 'send');
+    const payload = { hitType: 'pageview', page: '/blog' };
+    render(
+      <Root store={store}>
+        <BlogPage/>
+      </Root>
+    );
+
+    expect(spy).toBeCalledTimes(1);
+    expect(spy).toBeCalledWith(payload);
+  });
 });
