0653c47f8c03a76cabad5cecef0c84f10878c604
chunyukuo88 <alexgochenour@gmail.com>
2023-02-17 17:03:12 -0500

    Manual testing complete.

--- src/features/auth/RequireAuth.jsx
+++ src/features/auth/RequireAuth.jsx
@@ -1,4 +1,4 @@
-import {useLocation, Navigate, Outlet} from 'react-router-dom';
+import { useLocation, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectCurrentToken } from './authSlice';
 import { routes } from '../../routes';
