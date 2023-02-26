219084a864fdd1105dd83822f0b0c180fe461df6
chunyukuo88 <alexgochenour@gmail.com>
2023-02-25 19:03:38 -0500

    hover effect implemented with React

--- src/components/SkillsPole/SkillsPole.jsx
+++ src/components/SkillsPole/SkillsPole.jsx
@@ -1,3 +1,4 @@
+import { useState} from 'react';
 import ReduxIcon from '../../common/icons/tech_redux-icon.svg';
 import LambdaIcon from '../../common/icons/tech_lambda-icon.svg'
 import NodeIcon from '../../common/icons/tech_node-js-icon.svg';
@@ -7,57 +8,58 @@ import ServerlessIcon from '../../common/icons/tech_serverless-icon.svg';
 import './SkillsPole.css';
 
 export function SkillsPole(){
+  const [displayRedux, setDisplayRedux] = useState(true);
+  const [displayLambda, setDisplayLambda] = useState(true);
+  const [displayNode, setDisplayNode] = useState(true);
+  const [displaySvelte, setDisplaySvelte] = useState(true);
+  const [displayDocker, setDisplayDocker] = useState(true);
+  const [displaySls, setDisplaySls] = useState(true);
+
+
   return (
     <ul id='skills-pole'>
-      <li className='skill-block'>
-        React + Redux
-        <span><img
-          className='tech-icons'
-          src={ReduxIcon}
-          alt='redux icon'
-        /></span>
+      <li
+        onMouseEnter={() => setDisplayRedux(false)}
+        onMouseLeave={() => setDisplayRedux(true)}
+        className='skill-block'
+      >
+        {displayRedux ? 'React + Redux' : <span><img className='tech-icons' src={ReduxIcon} alt='redux icon'/></span>}
       </li>
-      <li className='skill-block'>
-        AWS
-        <span><img src={LambdaIcon} alt="aws-icon"/></span>
+      <li
+        onMouseEnter={() => setDisplayLambda(false)}
+        onMouseLeave={() => setDisplayLambda(true)}
+        className='skill-block'
+      >
+        {displayLambda ? 'AWS' : <span><img className='tech-icons' src={LambdaIcon} alt='aws-icon'/></span>}
       </li>
-      <li className='skill-block'>
-        Node.js
-        <span>
-          <img
-            src={NodeIcon}
-            alt="node-icon"
-          />
-        </span>
+      <li
+        onMouseEnter={() => setDisplayNode(false)}
+        onMouseLeave={() => setDisplayNode(true)}
+        className='skill-block'
+      >
+        {displayNode ? 'Node.js' : <span><img className='tech-icons' src={NodeIcon} alt='node-icon'/></span>}
       </li>
-      <li className='skill-block'>
-        Svelte
-        <span>
-          <img
-            src={SvelteIcon}
-            alt="svelte-icon"
-          />
-        </span>
+      <li
+        onMouseEnter={() => setDisplaySvelte(false)}
+        onMouseLeave={() => setDisplaySvelte(true)}
+        className='skill-block'
+      >
+        {displaySvelte ? 'Svelte' : <span><img className='tech-icons' src={SvelteIcon} alt='svelte-icon'/></span>}
       </li>
-      <li className='skill-block'>
-        Docker
-        <span>
-          <img
-            src={DockerIcon}
-            alt="docker-icon"
-          />
-        </span>
+      <li
+        onMouseEnter={() => setDisplayDocker(false)}
+        onMouseLeave={() => setDisplayDocker(true)}
+        className='skill-block'
+      >
+        {displayDocker ? 'Docker' : <span><img className='tech-icons' src={DockerIcon} alt='docker-icon'/></span>}
       </li>
-      <li className='skill-block'>
-        Serverless Framework
-        <span>
-          <img
-            src={ServerlessIcon}
-            alt="serverless-icon"
-          />
-        </span>
+      <li
+        onMouseEnter={() => setDisplaySls(false)}
+        onMouseLeave={() => setDisplaySls(true)}
+        className='skill-block'
+      >
+        {displaySls ? 'Serverless Framework' : <span><img className='tech-icons' src={ServerlessIcon} alt='serverless-icon' /></span>}
       </li>
-      <li className='skill-block'></li>
     </ul>
   );
 }
