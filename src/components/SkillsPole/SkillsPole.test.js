88e255b5b07bb61b4434edaf8bc2c2e6b9816e64
chunyukuo88 <alexgochenour@gmail.com>
2023-02-27 00:59:11 -0500

    WIP: Do SkillsPole next.

--- src/components/SkillsPole/SkillsPole.test.js
+++ src/components/SkillsPole/SkillsPole.test.js
@@ -1,6 +1,7 @@
 import { SkillsPole } from './SkillsPole';
 import { render } from '@testing-library/react';
 
+// TODO: Now that each block on the pole has its own handlers, they need tests.
 it('The component renders', () => {
   render(<SkillsPole />);
 
