d3e671ff3dc57ef7f1f0609e1b233e03550b80ca
chunyukuo88 <alexgochenour@gmail.com>
2023-03-18 21:32:20 -0400

    Improved accessibility for the AboutBlock component. Added aria-labelledby attribute to the newly added article tag, as well as h4 and h5, a role attribute to the outermost div tag, and an id for h4 that corresponds to the aria-labelledby attribute.

--- src/components/AboutBlock/AboutBlock.jsx
+++ src/components/AboutBlock/AboutBlock.jsx
@@ -3,15 +3,17 @@ import './AboutBlock.css';
 
 export function AboutBlock(){
   return (
-    <div id='about-block-wrapper'>
+    <div id='about-block-wrapper' role='complementary'>
       <div id='shadow'>
         <ContactLinks />
-        <section className='about-block'>
-          <div className='about-block-content-wrapper'>
-            <p>Hi! I'm Alex Gochenour.</p>
-            <p>I'm a full-stack JavaScript developer deploying to Amazon Web Services (AWS) using the Serverless Framework and sometimes EC2. I'm passionate about best practices: TDD, pair programming, and Scrum are where it's at.</p>
-          </div>
-        </section>
+        <article className='about-block' aria-labelledby='about-heading'>
+          <section className='about-block-content-wrapper'>
+            <header>
+              <h4 id='about-heading'>Hi! I'm Alex Gochenour.</h4>
+            </header>
+            <h5>I'm a full-stack JavaScript developer deploying to Amazon Web Services (AWS) using the Serverless Framework and sometimes EC2. I'm passionate about best practices: TDD, pair programming, and Scrum are where it's at.</h5>
+          </section>
+        </article>
       </div>
     </div>
   );
