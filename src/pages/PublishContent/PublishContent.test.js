0246454300615f5fdfd22c6813575b89dd7e4b91
chunyukuo88 <alexgochenour@gmail.com>
2023-04-08 18:03:01 -0400

    added tests.

--- src/pages/PublishContent/PublishContent.test.js
+++ src/pages/PublishContent/PublishContent.test.js
@@ -9,7 +9,7 @@ import '../../common/utils';
 
 import PublishContent from './PublishContent';
 
-jest.spyOn(window, 'alert').mockImplementation(() => {});
+const alertSpy = jest.spyOn(window, 'alert').mockImplementation(jest.fn());
 let mockFn = jest.fn();
 jest.mock('../../common/utils', () => {
   const originalModule = jest.requireActual('../../common/utils');
@@ -19,64 +19,104 @@ jest.mock('../../common/utils', () => {
     postData: () => mockFn(),
   };
 });
+describe('PublishContent.jsx', () => {
+  describe('GIVEN: the user wishes to publish a new crossword puzzle,', () => {
+    describe('WHEN: The page loads,', () => {
+      test('THEN: there is a panel allowing the user to publish new crosswords.', () => {
+        render(
+          <Provider store={mockStoreLoggedIn}>
+            <Router>
+              <PublishContent/>
+            </Router>
+          </Provider>
+        );
+        const publishPanel = screen.getByText('Create a New Crossword');
 
-describe('GIVEN: the user is logged in,', () => {
-  describe('WHEN: The page loads,', () => {
-    test('THEN: there is a panel allowing the user to publish new crosswords.', () => {
-      render(
-        <Provider store={mockStoreLoggedIn}>
-          <Router>
-            <PublishContent/>
-          </Router>
-        </Provider>
-      );
-      const publishPanel = screen.getByText('Create a New Crossword');
+        expect(publishPanel).toBeInTheDocument();
+      });
+    });
+    describe('WHEN: The has filled in all the crossword fields and submits,', () => {
+      test('THEN: the data they entered is sent to the API.', () => {
+        render(
+          <Provider store={mockStoreLoggedIn}>
+            <Router>
+              <PublishContent/>
+            </Router>
+          </Provider>
+        );
 
-      expect(publishPanel).toBeInTheDocument();
+        const solution = screen.getByTestId('crossword-panel-solution');
+        const title = screen.getByTestId('crossword-panel-title');
+        const theme = screen.getByTestId('crossword-panel-theme');
+        const across1 = screen.getByTestId('crossword-panel-across-1');
+        const across2 = screen.getByTestId('crossword-panel-across-2');
+        const across3 = screen.getByTestId('crossword-panel-across-3');
+        const across4 = screen.getByTestId('crossword-panel-across-4');
+        const across5 = screen.getByTestId('crossword-panel-across-5');
+        const down1 = screen.getByTestId('crossword-panel-down-1');
+        const down2 = screen.getByTestId('crossword-panel-down-2');
+        const down3 = screen.getByTestId('crossword-panel-down-3');
+        const down4 = screen.getByTestId('crossword-panel-down-4');
+        const down5 = screen.getByTestId('crossword-panel-down-5');
+        const button = screen.getAllByText('Publish')[1];
+
+        fireEvent.change(solution, { target: { value: '1234512345123451234512345' } });
+        fireEvent.change(title, { target: { value: 'title' } });
+        fireEvent.change(theme, { target: { value: 'theme' } });
+        fireEvent.change(across1, { target: { value: 'The numbers 12345' } });
+        fireEvent.change(across2, { target: { value: 'The numbers 12345 a second time' } });
+        fireEvent.change(across3, { target: { value: 'The numbers 12345 yet again' } });
+        fireEvent.change(across4, { target: { value: 'The numbers 12345 a fourth time' } });
+        fireEvent.change(across5, { target: { value: 'The numbers 12345 one last time' } });
+        fireEvent.change(down1, { target: { value: 'the numbers 11111' } });
+        fireEvent.change(down2, { target: { value: 'the numbers 22222' } });
+        fireEvent.change(down3, { target: { value: 'the numbers 33333' } });
+        fireEvent.change(down4, { target: { value: 'the numbers 44444' } });
+        fireEvent.change(down5, { target: { value: 'the numbers 55555' } });
+        fireEvent.click(button);
+
+        expect(mockFn).toBeCalledTimes(1);
+      });
     });
   });
-  describe('WHEN: The has filled in all the fields and submits,', () => {
-    test('THEN: the data they entered is sent to the API.', () => {
-      render(
-        <Provider store={mockStoreLoggedIn}>
-          <Router>
-            <PublishContent/>
-          </Router>
-        </Provider>
-      );
+  describe('GIVEN: the user wishes to publish a blog post,', () => {
+    describe('WHEN: they submit the filled out form', () => {
+      let title, body, imageUrl;
 
-      const solution = screen.getByTestId('crossword-panel-solution');
-      const title = screen.getByTestId('crossword-panel-title');
-      const theme = screen.getByTestId('crossword-panel-theme');
-      const across1 = screen.getByTestId('crossword-panel-across-1');
-      const across2 = screen.getByTestId('crossword-panel-across-2');
-      const across3 = screen.getByTestId('crossword-panel-across-3');
-      const across4 = screen.getByTestId('crossword-panel-across-4');
-      const across5 = screen.getByTestId('crossword-panel-across-5');
-      const down1 = screen.getByTestId('crossword-panel-down-1');
-      const down2 = screen.getByTestId('crossword-panel-down-2');
-      const down3 = screen.getByTestId('crossword-panel-down-3');
-      const down4 = screen.getByTestId('crossword-panel-down-4');
-      const down5 = screen.getByTestId('crossword-panel-down-5');
-      const button = screen.getAllByText('Publish')[1];
+      beforeEach(() => {
+        render(
+            <Provider store={mockStoreLoggedIn}>
+              <Router>
+                <PublishContent/>
+              </Router>
+            </Provider>
+        );
 
-      fireEvent.change(solution, { target: { value: '1234512345123451234512345' } });
-      fireEvent.change(title, { target: { value: 'title' } });
-      fireEvent.change(theme, { target: { value: 'theme' } });
-      fireEvent.change(across1, { target: { value: 'The numbers 12345' } });
-      fireEvent.change(across2, { target: { value: 'The numbers 12345 a second time' } });
-      fireEvent.change(across3, { target: { value: 'The numbers 12345 yet again' } });
-      fireEvent.change(across4, { target: { value: 'The numbers 12345 a fourth time' } });
-      fireEvent.change(across5, { target: { value: 'The numbers 12345 one last time' } });
-      fireEvent.change(down1, { target: { value: 'the numbers 11111' } });
-      fireEvent.change(down2, { target: { value: 'the numbers 22222' } });
-      fireEvent.change(down3, { target: { value: 'the numbers 33333' } });
-      fireEvent.change(down4, { target: { value: 'the numbers 44444' } });
-      fireEvent.change(down5, { target: { value: 'the numbers 55555' } });
-      fireEvent.click(button);
+        title = screen.getByTestId('blog-panel-title');
+        body = screen.getByTestId('blog-panel-body');
+        imageUrl = screen.getByTestId('blog-panel-img');
 
-      expect(mockFn).toBeCalledTimes(1);
+        fireEvent.change(title, { target: { value: 'some title' } });
+        fireEvent.change(body, { target: { value: 'some body' } });
+        fireEvent.change(imageUrl, { target: { value: 'some imageUrl' } });
+
+        const submissionButton = screen.getByTestId('blog-submission-btn');
+
+        fireEvent.click(submissionButton);
+      });
+      it('THEN: that data is sent to the Lambda.', () => {
+        expect(mockFn).toBeCalledTimes(1);
+      });
+      it('THEN: a success message is alerted to the user.', () => {
+        expect(alertSpy).toBeCalledTimes(1);
+      });
+      it('THEN: the inputs are cleared.', () => {
+        title = screen.getByTestId('blog-panel-title');
+        body = screen.getByTestId('blog-panel-body');
+        imageUrl = screen.getByTestId('blog-panel-img');
+
+        expect(title).toHaveTextContent('');
+      });
     });
-    // TODO: Write a test that asserts that `postData` is invoked with collated clues.
   });
 });
