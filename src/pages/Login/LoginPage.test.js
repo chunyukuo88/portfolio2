52249bcf4d5b4e00caf2c4751459dc43afbc552e
chunyukuo88 <alexgochenour@gmail.com>
2023-04-24 22:09:31 -0400

    refactor: Tried using a test matrix but that broke everything for strange reasons. Added 'it' block description.

--- src/pages/Login/LoginPage.test.js
+++ src/pages/Login/LoginPage.test.js
@@ -55,67 +55,67 @@ describe('GIVEN: user is NOT logged in', () => {
 
       expect(page).toBeInTheDocument();
     });
-  });
-  it('', async () => {
-    const baseStyleRules = {
-      position: 'relative',
-      transform: 'rotateX(-30deg)',
-      animation: 'animate 4s linear infinite',
-      transition: '3s',
-    };
-    const backButton = document.querySelector('.back-to-home');
-    const small = { height: '100px', width: '100px' };
-    const lessSmall = { height: '150px', width: '150px' };
-    const notSmall = { height: '200px', width: '200px' };
-    const significant = { height: '250px', width: '250px' };
-    const big = { height: '300px', width: '300px' };
-    const bigg = { height: '350px', width: '350px' };
-    const bigger = { height: '400px', width: '400px' };
-    const evenBigger = { height: '450px', width: '450px' };
+    it('THEN: the position of the cursor affects the size and color of the cube.', async () => {
+      const baseStyleRules = {
+        position: 'relative',
+        transform: 'rotateX(-30deg)',
+        animation: 'animate 4s linear infinite',
+        transition: '3s',
+      };
+      const backButton = document.querySelector('.back-to-home');
+      const small = { height: '100px', width: '100px' };
+      const lessSmall = { height: '150px', width: '150px' };
+      const notSmall = { height: '200px', width: '200px' };
+      const significant = { height: '250px', width: '250px' };
+      const big = { height: '300px', width: '300px' };
+      const bigg = { height: '350px', width: '350px' };
+      const bigger = { height: '400px', width: '400px' };
+      const evenBigger = { height: '450px', width: '450px' };
 
-    fireEvent.mouseOver(backButton);
-    let cube = document.querySelector('.cube');
-    expect(cube).toHaveStyle({ ...baseStyleRules, ...small });
-
-    await waitFor(() => {
-      fireEvent.mouseMove(document, { clientY: 50 });
-      cube = document.querySelector('.cube');
+      fireEvent.mouseOver(backButton);
+      let cube = document.querySelector('.cube');
       expect(cube).toHaveStyle({ ...baseStyleRules, ...small });
-    });
-    await waitFor(() => {
-      fireEvent.mouseMove(document, { clientY: 100 });
-      cube = document.querySelector('.cube');
-      expect(cube).toHaveStyle({ ...baseStyleRules, ...lessSmall });
-    });
-    await waitFor(() => {
-      fireEvent.mouseMove(document, { clientY: 150 });
-      cube = document.querySelector('.cube');
-      expect(cube).toHaveStyle({ ...baseStyleRules, ...notSmall });
-    });
-    await waitFor(() => {
-      fireEvent.mouseMove(document, { clientY: 200 });
-      cube = document.querySelector('.cube');
-      expect(cube).toHaveStyle({ ...baseStyleRules, ...significant });
-    });
-    await waitFor(() => {
-      fireEvent.mouseMove(document, { clientY: 250 });
-      cube = document.querySelector('.cube');
-      expect(cube).toHaveStyle({ ...baseStyleRules, ...big });
-    });
-    await waitFor(() => {
-      fireEvent.mouseMove(document, { clientY: 300 });
-      cube = document.querySelector('.cube');
-      expect(cube).toHaveStyle({ ...baseStyleRules, ...bigg });
-    });
-    await waitFor(() => {
-      fireEvent.mouseMove(document, { clientY: 350 });
-      cube = document.querySelector('.cube');
-      expect(cube).toHaveStyle({ ...baseStyleRules, ...bigger });
-    });
-    await waitFor(() => {
-      fireEvent.mouseMove(document, { clientY: 400 });
-      cube = document.querySelector('.cube');
-      expect(cube).toHaveStyle({ ...baseStyleRules, ...evenBigger });
+
+      await waitFor(() => {
+        fireEvent.mouseMove(document, { clientY: 50 });
+        cube = document.querySelector('.cube');
+        expect(cube).toHaveStyle({ ...baseStyleRules, ...small });
+      });
+      await waitFor(() => {
+        fireEvent.mouseMove(document, { clientY: 100 });
+        cube = document.querySelector('.cube');
+        expect(cube).toHaveStyle({ ...baseStyleRules, ...lessSmall });
+      });
+      await waitFor(() => {
+        fireEvent.mouseMove(document, { clientY: 150 });
+        cube = document.querySelector('.cube');
+        expect(cube).toHaveStyle({ ...baseStyleRules, ...notSmall });
+      });
+      await waitFor(() => {
+        fireEvent.mouseMove(document, { clientY: 200 });
+        cube = document.querySelector('.cube');
+        expect(cube).toHaveStyle({ ...baseStyleRules, ...significant });
+      });
+      await waitFor(() => {
+        fireEvent.mouseMove(document, { clientY: 250 });
+        cube = document.querySelector('.cube');
+        expect(cube).toHaveStyle({ ...baseStyleRules, ...big });
+      });
+      await waitFor(() => {
+        fireEvent.mouseMove(document, { clientY: 300 });
+        cube = document.querySelector('.cube');
+        expect(cube).toHaveStyle({ ...baseStyleRules, ...bigg });
+      });
+      await waitFor(() => {
+        fireEvent.mouseMove(document, { clientY: 350 });
+        cube = document.querySelector('.cube');
+        expect(cube).toHaveStyle({ ...baseStyleRules, ...bigger });
+      });
+      await waitFor(() => {
+        fireEvent.mouseMove(document, { clientY: 400 });
+        cube = document.querySelector('.cube');
+        expect(cube).toHaveStyle({ ...baseStyleRules, ...evenBigger });
+      });
     });
   });
 });
