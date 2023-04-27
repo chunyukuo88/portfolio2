7f82ba7c444b834dcdafbe1556531896e6493b92
chunyukuo88 <alexgochenour@gmail.com>
2023-04-26 20:17:57 -0400

    refactor: StrictMode

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Root from './Root';
@@ -11,9 +12,9 @@ Auth.configure({
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
     <Root store={store}>
       <App />
     </Root>
-  </React.StrictMode>,
+  </StrictMode>,
 );
