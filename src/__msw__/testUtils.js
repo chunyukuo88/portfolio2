2bb36d5fc50e0c7f9c13b7e3811196942769de97
chunyukuo88 <alexgochenour@gmail.com>
2023-06-06 23:32:59 -0400

    feat: JS Docs! Added the correct types for __msw__/testUtils.js

--- src/__msw__/testUtils.js
+++ src/__msw__/testUtils.js
@@ -1,5 +1,8 @@
 import { render } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from 'react-redux';
+import { EnhancedStore } from '@reduxjs/toolkit';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ReactElement } from 'react';
 
 const generateQueryClient = () => {
@@ -9,14 +12,19 @@ const generateQueryClient = () => {
 /**
  * A wrapper for React components that acts similarly to Root but is used for tests only.
  * @param { ReactElement } reactElement - The component being tested.
- * @param { QueryClient } client - Required to be able to access React Query via msw.
+ * @param { EnhancedStore } store - A mock Redux store, representing authenticated and non-authenticated states.
+ * @param { QueryClient } [client] - To access React Query via msw.
  */
-export function renderWithQueryClient(reactElement, client) {
+export function renderWithQueryClient(reactElement, store, client) {
   const queryClient = client ?? generateQueryClient();
 
-  return render(<
-    QueryClientProvider client={queryClient}>
-      {reactElement}
+  return render(
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Router>
+          {reactElement}
+        </Router>
+      </Provider>
     </QueryClientProvider>
   );
 }
