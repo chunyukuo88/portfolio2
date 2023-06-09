007a3d7b3ca996bb22e7c086c5122432e2f2e5b3
chunyukuo88 <alexgochenour@gmail.com>
2023-06-08 22:28:12 -0400

    Fixed Pencil.test.js

--- src/__tests__/components/Pencil/Pencil.test.js
+++ src/__tests__/components/Pencil/Pencil.test.js
@@ -1,41 +1,60 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
+import { renderWithQueryClient } from 'src/__msw__/testUtils';
+import { mockStoreLoggedIn } from 'src/testUtils';
+
 import { Pencil } from 'src/components/Pencil/Pencil';
 import { updateBlogPost } from 'src/common/utils';
 
-import { mockStoreLoggedIn } from 'src/testUtils';
-import Root from 'src/Root';
-
-jest.mock('src/common/utils');
+jest.mock('src/common/utils', () => {
+  const originalModule = jest.requireActual('src/common/utils');
+  return {
+    __esModule: true,
+    ...originalModule,
+    updateBlogPost: jest.fn(),
+  }
+});
 jest.spyOn(console, 'log').mockImplementation(jest.fn());
 
 describe('GIVEN: an entityId and a blog post aspect (title, body, or imgUrl),', () => {
   describe('WHEN: the user clicks the pencil then confirms the updated aspect,', () => {
-    test.skip('THEN: the change is sent to the back end.', async() => {
-      updateBlogPost.mockImplementationOnce(() => {});
+    test('THEN: the change is sent to the back end.', async() => {
+      const article = {
+        entityId: 123,
+        title: 'some title',
+      };
       const aspect = 'title';
       const entityId = '123';
       const newTitle = 'a new title!';
-      const expectedUpdate = {
-        title: newTitle,
+      const expectedUpdate = { title: newTitle };
+      const requestContainingExpectedUpdate = {
+        body: "{\"title\":\"a new title!\"}",
+        headers: {
+          Authorization: 'Bearer undefined',
+          'Content-Type': 'application/json',
+        },
+        method: 'PUT',
       };
-      render(
-        <Root store={mockStoreLoggedIn}>
-          <Pencil aspect={aspect} entityId={entityId}/>
-        </Root>
+      renderWithQueryClient(
+        <Pencil
+          aspect={aspect}
+          entityId={entityId}
+          article={article}
+        />, mockStoreLoggedIn
       );
 
       const pencil = screen.queryByText('✏️');
       fireEvent.click(pencil);
 
       const textBox = screen.queryByRole('textbox');
-      const confirmationButton = screen.queryByText('Confirm');
-
       fireEvent.change(textBox, { target: { value: expectedUpdate.title } });
 
+      const confirmationButton = screen.queryByText('Confirm');
       fireEvent.click(confirmationButton);
 
-      expect(updateBlogPost).toBeCalledTimes(1);
-      expect(updateBlogPost).toBeCalledWith(expectedUpdate);
+      await waitFor(() => {
+        expect(updateBlogPost).toBeCalledTimes(1);
+        expect(updateBlogPost).toBeCalledWith(article.entityId, requestContainingExpectedUpdate);
+      });
     });
   });
 });
