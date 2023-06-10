ac00471c1e883a9ea9e767e66e013ad503c375ed
chunyukuo88 <alexgochenour@gmail.com>
2023-06-09 22:48:52 -0400

    Feature complete for now.

--- src/pages/Workouts/Workouts.js
+++ src/pages/Workouts/Workouts.js
@@ -1,1 +1,28 @@
-export function Workouts(){}
+import { LinkStyling } from 'src/common/globalStyles';
+import { useCommonGlobals } from 'src/common/hooks';
+import strings from 'src/common/strings';
+import { Link } from 'react-router-dom';
+import { routes } from 'src/routes';
+
+export function Workouts(){
+  const [ language ] = useCommonGlobals(routes.workouts);
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    color: 'white',
+    fontSize: '3rem',
+  };
+  return (
+    <>
+      <nav className='back-to-home'>
+        <Link style={LinkStyling} to={routes.index}>
+          {strings.backButton[language]}
+        </Link>
+      </nav>
+      <div style={style}>
+        Coming soon!
+      </div>
+    </>
+  );
+}
