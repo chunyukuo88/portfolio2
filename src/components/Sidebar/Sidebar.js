4e8c7dad69b430990ce11e62c7b1a7bbd9f88daa
chunyukuo88 <alexgochenour@gmail.com>
2023-06-11 23:18:48 -0400

    Sidebar done.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,13 +1,16 @@
 import { CSSTransition } from 'react-transition-group';
 import strings from 'src/common/strings';
+import { useCommonGlobals } from 'src/common/hooks';
+import { routes } from 'src/routes';
 import './Sidebar.css';
 
 export function Sidebar({ isOpen }){
+  const [ language ] = useCommonGlobals(routes.index);
   const menuItems = [
-    { title: strings.aboutMe[strings.ENGLISH] },
-    { title: strings.aboutSite[strings.ENGLISH] },
-    { title: strings.resume[strings.ENGLISH] },
-    { title: strings.funStuff[strings.ENGLISH] },
+    { title: strings.aboutMe[language] },
+    { title: strings.aboutSite[language] },
+    { title: strings.resume[language] },
+    { title: strings.funStuff[language] },
   ];
 
   return (
