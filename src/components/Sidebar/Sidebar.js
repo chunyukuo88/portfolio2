9663472f42c618f4bc78f5e2200eb0e20253f5f2
chunyukuo88 <alexgochenour@gmail.com>
2023-06-11 22:43:59 -0400

    Feat: Sidebar works. That was a lot less painful that I thought.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,11 +1,13 @@
 import { CSSTransition } from 'react-transition-group';
 import strings from 'src/common/strings';
+import './Sidebar.css';
 
-export function Sidebar({ isOpen, setIsOpen }){
+export function Sidebar({ isOpen }){
   const menuItems = [
     { title: strings.aboutMe[strings.ENGLISH] },
     { title: strings.aboutSite[strings.ENGLISH] },
     { title: strings.resume[strings.ENGLISH] },
+    { title: strings.funStuff[strings.ENGLISH] },
   ];
 
   return (
