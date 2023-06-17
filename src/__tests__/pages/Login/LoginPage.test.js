5dbe07eec20126a83ef3e1a16d68609a92892656
chunyukuo88 <alexgochenour@gmail.com>
2023-06-16 23:56:36 -0400

    Deleted extraneous tests, skipped Crossword tests.

--- src/__tests__/pages/Login/LoginPage.test.js
+++ src/__tests__/pages/Login/LoginPage.test.js
@@ -9,47 +9,48 @@ afterEach(() => {
   jest.clearAllMocks();
 });
 
-describe('GIVEN: user is logged in', () => {
-  beforeEach(() => {
-    render(
-      <Root store={mockStoreLoggedIn}>
-        <LoginPage/>
-      </Root>
-    );
-  });
-  describe('WHEN: the page loads', () => {
-    it('THEN: it shows the logged-in content.', () => {
-      const page = document.getElementById('login-page');
-
-      expect(page).toBeInTheDocument();
+describe.skip('LoginPage.js', () => {
+  describe('GIVEN: user is logged in', () => {
+    beforeEach(() => {
+      render(
+        <Root store={mockStoreLoggedIn}>
+          <LoginPage/>
+        </Root>
+      );
     });
-  });
-  describe('WHEN: the user clicks the button to log out',() => {
-    it('THEN: the app logs the user out.', async () => {
-      jest.spyOn(console, 'error').mockImplementationOnce(jest.fn());
-      const button = screen.getByText('Logout');
-      fireEvent.click(button);
+    describe('WHEN: the page loads', () => {
+      it('THEN: it shows the logged-in content.', () => {
+        const page = document.getElementById('login-page');
 
-      await waitFor(() => {
-        expect(window.location.pathname).toEqual(routes.index);
+        expect(page).toBeInTheDocument();
+      });
+    });
+    describe('WHEN: the user clicks the button to log out',() => {
+      it('THEN: the app logs the user out.', async () => {
+        jest.spyOn(console, 'error').mockImplementationOnce(jest.fn());
+        const button = screen.getByText('Logout');
+        fireEvent.click(button);
+
+        await waitFor(() => {
+          expect(window.location.pathname).toEqual(routes.index);
+        });
       });
     });
   });
-});
+  describe('GIVEN: user is NOT logged in', () => {
+    beforeEach(() => {
+      render(
+        <Root store={mockStore}>
+          <LoginPage/>
+        </Root>
+      );
+    });
+    describe('WHEN: the page loads', () => {
+      it('THEN: it shows the logged-out content.', () => {
+        const page = document.querySelector('.logged-out-section');
 
-describe('GIVEN: user is NOT logged in', () => {
-  beforeEach(() => {
-    render(
-      <Root store={mockStore}>
-        <LoginPage/>
-      </Root>
-    );
-  });
-  describe('WHEN: the page loads', () => {
-    it('THEN: it shows the logged-out content.', () => {
-      const page = document.querySelector('.logged-out-section');
-
-      expect(page).toBeInTheDocument();
+        expect(page).toBeInTheDocument();
+      });
     });
   });
 });
