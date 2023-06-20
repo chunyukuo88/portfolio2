8d7e69a449992b4a41b48943f4c13396a8cd90d3
chunyukuo88 <alexgochenour@gmail.com>
2023-06-19 22:59:00 -0400

    Updated content.

--- src/components/PrimaryContent/SiteInfo/strings.js
+++ src/components/PrimaryContent/SiteInfo/strings.js
@@ -6,9 +6,9 @@ export const strings = {
   },
   abridgedVersionBody: {
     chinese: '',
-    english: 'This is a test-driven website made with: Supabase, Redis Cloud, AWS Lambdas\n' +
+    english: 'This is a test-driven website made with: Supabase, Redis Cloud, AWS Lambdas,\n' +
       'the Serverless Framework, React, Tanstack Query, Redux, and msw (Mock Service Worker). All the CSS,\n' +
-      'including the animations and transitions are scratch-made with the exception of CSSTransitions.',
+      'including the animations and transitions, are scratch-made--with the exception of CSSTransitions for the side bar.',
     german: '',
   },
   unabridgedVersionTitle: {
@@ -20,26 +20,27 @@ export const strings = {
     chinese: '',
     english: 'This is a test-driven website made with a host of interesting technologies: Supabase, Redis Cloud, Lambda\n' +
       'functions, the Serverless Framework, React, Redux, and tested with msw (Mock Service Worker). All the CSS,\n' +
-      'including the animations and transitions are scratch-made with the exception of CSSTransitions.',
+      'including the animations and transitions, are scratch-made--with the exception of CSSTransitions for the side bar.',
     german: '',
   },
   paragraph1: {
     chinese: '',
-    english: 'My motivation to create this site was to apply the technologies I work with to make a site that expresses\n' +
-      'my own design preferences and functionality. There are, of course, a few baubles like Dark Mode and the spinning\n' +
-      'cube. While those are made with vanilla CSS and Redux, they were a blast to assemble.',
+    english: 'My motivation to create this site was two-fold: I wanted to apply the technologies I work with to make a site that expresses\n' +
+      'my own design preferences and functionality and towards features I have never been paid to produce; I also wanted to explore technologies I have never been paid to use, such as Supabase, Redux Toolkit, msw, and Redis. There are, of course, a few baubles like Dark Mode and the spinning\n' +
+      'cube that found themselves creeping into the project along the way. (The cube was made with vanilla CSS and Redux, and it was a blast to assemble.)',
     german: '',
   },
   paragraph2: {
     chinese: '',
-    english: 'The first iteration of this site was a paean to cubic design. Without a designer, that quickly turned into a\n' +
-      'scattered mess. A friend remarked that it looked like I had just discovered how to animate three-dimensional\n' +
+    english: 'The first iteration of this site was a paean to cubic design. Without a designer, that quickly turned into an\n' +
+      'amateurish mess. A friend remarked that it looked like I had just discovered how to animate three-dimensional\n' +
       'objects with just CSS3 and went to town with it without any thought to a cohesive UX experience. They were right.',
     german: '',
   },
   paragraph3: {
     chinese: '',
-    english: 'Hence the present iteration, which dispenses with the React Spring, bright colors, and (excess) flashy animations.\n' +
+    english: 'Hence the present iteration, which dispenses with the React Spring, bright colors, and (excess) flashy animations and is ' +
+      'informed a Udemy course as well as many, many Design Course videos.\n' +
       'The front-end code is publicly available, but the back end is private, as I use it to experiment a lot with security features beyond\n' +
       'the standard protected routes configuration that the Serverless Framework offers. Because of this, I cannot make it publicly available,\n' +
       'although I would be happy to provide a guided tour on a case-by-case basis. It consists of a suite of Lambda functions written in Node\n' +
@@ -48,7 +49,7 @@ export const strings = {
   },
   thanksForStoppingBy: {
     chinese: '',
-    english: 'Thanks for stopping by!',
+    english: 'Oh wow you read to the very end. Let me know and I will bake you a loaf of bread. Thanks for stopping by!',
     german: '',
   },
 };
