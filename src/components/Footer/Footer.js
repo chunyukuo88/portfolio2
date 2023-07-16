8b2e2226d689412817fcc15ad7369e572a5f291f
chunyukuo88 <alexgochenour@gmail.com>
2023-07-16 17:30:25 -0400

    Re-added back rule sets as welll as the Cube component.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,21 +7,21 @@ export function Footer() {
   return (
     <footer>
       <span>
-        <img className='main-icons' src={Mail} alt='Mail icon'/>
+        <img className='footer-icons' src={Mail} alt='Mail icon'/>
       </span>
     <span>
       <a href='https://github.com/chunyukuo88/' target='_blank' rel='noreferrer'>
-        <img className='main-icons' src={GitHub} alt='Github icon'/>
+        <img className='footer-icons' src={GitHub} alt='Github icon'/>
       </a>
     </span>
       <span>
         <a href='https://www.npmjs.com/package/gochenour' target='_blank' rel='noreferrer'>
-          <img className='main-icons' src={NPM} alt='NPM icon'/>
+          <img className='footer-icons' src={NPM} alt='NPM icon'/>
         </a>
       </span>
       <span>
         <a href='https://www.linkedin.com/in/alex-gochenour/' target='_blank' rel='noreferrer'>
-          <img className='main-icons' src={LinkedIn} alt='LinkedIn icon'/>
+          <img className='footer-icons' src={LinkedIn} alt='LinkedIn icon'/>
         </a>
       </span>
     </footer>
