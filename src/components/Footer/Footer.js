8b8dbcc1e65b50963ada02e5e17e3b078519c873
chunyukuo88 <alexgochenour@gmail.com>
2023-07-17 23:12:00 -0400

    Added an indent and a top border to the footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,21 +6,21 @@ import LinkedIn from 'src/common/icons/contact_LinkedIn.svg';
 export function Footer() {
   return (
     <footer>
-    <span>
-      <a href='https://github.com/chunyukuo88/' target='_blank' rel='noreferrer'>
-        <img className='footer-icons' src={GitHub} alt='Github icon'/>
-      </a>
-    </span>
       <span>
-        <a href='https://www.npmjs.com/package/gochenour' target='_blank' rel='noreferrer'>
-          <img className='footer-icons' src={NPM} alt='NPM icon'/>
+        <a href='https://github.com/chunyukuo88/' target='_blank' rel='noreferrer'>
+          <img className='footer-icons' src={GitHub} alt='Github icon'/>
         </a>
       </span>
-      <span>
-        <a href='https://www.linkedin.com/in/alex-gochenour/' target='_blank' rel='noreferrer'>
-          <img className='footer-icons' src={LinkedIn} alt='LinkedIn icon'/>
-        </a>
-      </span>
+        <span>
+          <a href='https://www.npmjs.com/package/gochenour' target='_blank' rel='noreferrer'>
+            <img className='footer-icons' src={NPM} alt='NPM icon'/>
+          </a>
+        </span>
+        <span>
+          <a href='https://www.linkedin.com/in/alex-gochenour/' target='_blank' rel='noreferrer'>
+            <img className='footer-icons' src={LinkedIn} alt='LinkedIn icon'/>
+          </a>
+        </span>
     </footer>
   );
 }
