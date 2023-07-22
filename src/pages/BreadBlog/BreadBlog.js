e7ec34e66862de427df94c1b636a2a02128e9cdd
chunyukuo88 <alexgochenour@gmail.com>
2023-07-22 13:55:59 -0400

    First passing test for BreadBlog.test.js

--- src/pages/BreadBlog/BreadBlog.js
+++ src/pages/BreadBlog/BreadBlog.js
@@ -1,3 +1,13 @@
-export function BreadBlog() {
+import {useIntersection} from "react-use"; // TODO: check this out
+import './BreadBlog.css';
 
+export function BreadBlog({ menuIsOpen }) {
+  return (
+    <article id='bread-blog'>
+      <section>
+        <title></title>
+        <p className='blog-body'></p>
+      </section>
+    </article>
+  );
 }
