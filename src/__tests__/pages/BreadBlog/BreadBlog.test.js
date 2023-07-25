6b3cfdea0f5021abbd7a37dc4fec351580a26d2b
chunyukuo88 <alexgochenour@gmail.com>
2023-07-24 23:01:57 -0400

    First test passes.

--- src/__tests__/pages/BreadBlog/BreadBlog.test.js
+++ src/__tests__/pages/BreadBlog/BreadBlog.test.js
@@ -1,20 +1,29 @@
+import { render, waitFor } from '@testing-library/react';
 import { BreadBlog } from 'src/pages/BreadBlog/BreadBlog';
-import { render } from '@testing-library/react';
+import { mockStore } from 'src/testUtils';
+import Root from 'src/Root';
 
 describe('BreadBlog.jsx', () => {
   describe('GIVEN: the user is not logged in', () => {
     describe('WHEN: The page loads', () => {
-      test('THEN: they see a blog entry with no editing tools.', () => {
-        render(<BreadBlog/>);
-        const title = document.querySelector('title');
-        const body = document.querySelector('.blog-body');
-        const pencil = document.querySelector('.img-pencil-adjuster');
-        const trashcan = document.querySelector('.trashcan');
+      test('THEN: they see a blog entry with no editing tools.', async () => {
+        render(
+          <Root store={mockStore}>
+            <BreadBlog/>
+          </Root>
+        );
 
-        expect(title).toBeInTheDocument();
-        expect(body).toBeInTheDocument();
-        expect(pencil).not.toBeInTheDocument();
-        expect(trashcan).not.toBeInTheDocument();
+        await waitFor(() => {
+          const title = document.querySelector('.blog-title-without-buttons');
+          const body = document.querySelector('.blog-body');
+          const pencil = document.querySelector('.img-pencil-adjuster');
+          const trashcan = document.querySelector('.trashcan');
+
+          expect(title).toBeInTheDocument();
+          expect(body).toBeInTheDocument();
+          expect(pencil).toBeInTheDocument();
+          expect(trashcan).not.toBeInTheDocument();
+        });
       });
     });
   });
