bb02ca2594fb42bf7f7aed048d85f8f5ddf0ce11
chunyukuo88 <alexgochenour@gmail.com>
2023-09-29 18:28:27 -0400

    Created features/index.js, the barrel module.

---
+++ src/features/index.js
@@ -0,0 +1,6 @@
+export { selectCurrentUser, selectCurrentToken } from './auth/authSlice';
+export { updateLanguage } from './language/languageSlice';
+export { toggleToSpinQuickly, toggleToSpinSlowly } from './cubeSpin/cubeSpinSlice';
+export { setDarkMode, setLightMode } from './darkMode/darkModeSlice';
+export { updateGrid, declareVictory, resetGrid, resetVictoryState } from './crossword/crosswordSlice';
+export { updateSettingsVisibility } from './settingsMenu/settingsMenuSlice';
