f4a2509e2bacaae66a358eff33eeb08a70515df3
chunyukuo88 <alexgochenour@gmail.com>
2023-12-04 22:13:29 -0500

    Fixed tests, all of which now pass.

--- src/__tests__/pages/Blog/BreadBlogArticle.test.js
+++ src/__tests__/pages/Blog/BreadBlogArticle.test.js
@@ -1,7 +1,9 @@
-import { BreadBlogArticle } from 'src/pages/BreadBlog/BreadBlogArticle';
 import { render, screen, waitFor } from '@testing-library/react';
-import Root from 'src/Root';
 import { store } from 'src/globalState/store';
+import { storeWithUserLoggedIn } from 'src/__mocks__/mockStores';
+import Root from 'src/Root';
+
+import { BreadBlogArticle } from 'src/pages/BreadBlog/BreadBlogArticle';
 
 const article = {
   title: 'English Muffins',
@@ -13,36 +15,57 @@ const article = {
   views: 1
 };
 
-describe('GIVEN: passed an ordinary article with valid fields,', () => {
-  describe('WHEN: the image has not loaded,', () => {
-    test('THEN: it displays a spinner and the image is not visible.', () => {
+describe('BreadBlogArticle.jsx', () => {
+  describe('WHEN: the user is logged in', () => {
+    beforeEach(() => {
       render(
-        <Root store={store}>
+        <Root store={storeWithUserLoggedIn}>
           <BreadBlogArticle article={article} />
         </Root>
       );
+    });
+    it('THEN: The pencil icons are visible', () => {
+      const pencil = document.querySelector('svg');
 
-      const loadingSpinner = document.querySelector('.loading-spinner-container');
-      const imageAltText = screen.queryByAltText(/Image for blog titled/);
+      expect(pencil).toBeVisible();
+    });
+    it('THEN: The title has editing buttons', () => {
+      const titleWithButtons = document.querySelector('.title-with-buttons');
 
-      expect(loadingSpinner).toBeVisible();
-      expect(imageAltText).toBeInTheDocument();
+      expect(titleWithButtons).toBeVisible();
     });
   });
-  describe('WHEN: the img has not loaded,', () => {
-    test('THEN: the spinner goes away and the image appears', () => {
-      render(
-        <Root store={store}>
-          <BreadBlogArticle article={article} />
-        </Root>
-      );
+  describe('GIVEN: the user is not logged in,', () => {
+    describe('WHEN: the image has not loaded,', () => {
+      test('THEN: it displays a spinner and the image is not visible.', () => {
+        render(
+          <Root store={store}>
+            <BreadBlogArticle article={article} />
+          </Root>
+        );
 
-      const loadingSpinner = document.querySelector('.loading-spinner-container');
-
-      waitFor(() => {
-        expect(loadingSpinner).not.toBeVisible();
+        const loadingSpinner = document.querySelector('.loading-spinner-container');
         const imageAltText = screen.queryByAltText(/Image for blog titled/);
-        expect(imageAltText).not.toBeInTheDocument();
+
+        expect(loadingSpinner).toBeVisible();
+        expect(imageAltText).toBeInTheDocument();
+      });
+    });
+    describe('WHEN: the img has not loaded,', () => {
+      test('THEN: the spinner goes away and the image appears', () => {
+        render(
+          <Root store={store}>
+            <BreadBlogArticle article={article} />
+          </Root>
+        );
+
+        const loadingSpinner = document.querySelector('.loading-spinner-container');
+
+        waitFor(() => {
+          expect(loadingSpinner).not.toBeVisible();
+          const imageAltText = screen.queryByAltText(/Image for blog titled/);
+          expect(imageAltText).not.toBeInTheDocument();
+        });
       });
     });
   });
