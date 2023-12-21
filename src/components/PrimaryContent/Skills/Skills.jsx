97a73b2223d5c49a61ada6ae6e0dbd25b830b8db
chunyukuo88 <alexgochenour@gmail.com>
2023-12-21 15:59:25 -0500

    Nixed the hooks and replaced them with a pure CSS solution.

--- src/components/PrimaryContent/Skills/Skills.jsx
+++ src/components/PrimaryContent/Skills/Skills.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import strings from 'src/common/strings';
 import './Skills.css';
 
@@ -9,63 +8,23 @@ export function Skills(props) {
     ? 'tech-skills-abridged__blurry'
     : 'tech-skills-abridged';
 
-  const [serverlessStyle, setServerlessStyle] = useState('');
-  const [tddStyle, setTddStyle] = useState('');
-  const [lambdasStyle, setLambdasStyle] = useState('');
-  const [testingLibrary, setTestingLibrary] = useState('');
-  const [webComponents, setWebComponents] = useState('');
-  const [docker, setDocker] = useState('');
-  const [reactQuery, setReactQuery] = useState('');
-  const [redis, setRedis] = useState('');
-  const [svelte, setSvelte] = useState('');
-  const [supabase, setSupabase] = useState('');
-  const [gql, setGql] = useState('');
-  const [scrum, setScrum] = useState('');
-  const [react, setReact] = useState('');
-  const [msw, setMsw] = useState('');
-  const [css, setCss] = useState('');
-
-  useEffect(() => {
-    setTimeout(() => { setServerlessStyle('dynamic-emergence')}, 800);
-    setTimeout(() => { setTddStyle('dynamic-emergence')}, 1200);
-    setTimeout(() => { setLambdasStyle('dynamic-emergence')}, 1600);
-
-    setTimeout(() => { setTestingLibrary('dynamic-emergence')}, 2400);
-    setTimeout(() => { setWebComponents('dynamic-emergence')}, 2800);
-    setTimeout(() => { setDocker('dynamic-emergence')}, 3200);
-
-    setTimeout(() => { setReactQuery('dynamic-emergence')}, 3600);
-    setTimeout(() => { setRedis('dynamic-emergence')}, 4000);
-    setTimeout(() => { setSvelte('dynamic-emergence')}, 4400);
-
-    setTimeout(() => { setSupabase('dynamic-emergence')}, 4800);
-    setTimeout(() => { setGql('dynamic-emergence')}, 5200);
-    setTimeout(() => { setScrum('dynamic-emergence')}, 5600);
-
-    setTimeout(() => { setReact('dynamic-emergence')}, 6000);
-    setTimeout(() => { setMsw('dynamic-emergence')}, 6400);
-    setTimeout(() => { setCss('dynamic-emergence')}, 6800);
-
-    setTimeout(() => { setTddStyle('highlighted-skill')}, 7200);
-  }, []);
-
   return (
     <ul id={getId()} onClick={skillClickHandler}>
-      <li className={serverlessStyle}>The Serverless Framework</li>
-      <li className={tddStyle}>{strings.techSkillsTDD[language]}</li>
-      <li className={lambdasStyle}>Lambdas with Node.js</li>
-      <li className={testingLibrary}>Testing Library</li>
-      <li className={webComponents}>Web Components</li>
-      <li className={docker}>Docker Compose</li>
-      <li className={reactQuery}>React Query</li>
-      <li className={redis}>Redis Cloud</li>
-      <li className={svelte}>SvelteKit</li>
-      <li className={supabase}>Supabase</li>
-      <li className={gql}>GraphQL</li>
-      <li className={scrum}>Scrum</li>
-      <li className={react}>React</li>
-      <li className={msw}>msw</li>
-      <li className={css}>css</li>
+      <li>The Serverless Framework</li>
+      <li>{strings.techSkillsTDD[language]}</li>
+      <li>Lambdas with Node.js</li>
+      <li>Testing Library</li>
+      <li>Web Components</li>
+      <li>Docker Compose</li>
+      <li>React Query</li>
+      <li>Redis Cloud</li>
+      <li>SvelteKit</li>
+      <li>Supabase</li>
+      <li>GraphQL</li>
+      <li>Scrum</li>
+      <li>React</li>
+      <li>msw</li>
+      <li>css</li>
     </ul>
   );
 }
